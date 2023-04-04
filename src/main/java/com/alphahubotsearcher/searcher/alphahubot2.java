6556b0173fb0da18fa30f1f4c0304064bee1aee1
DICEALPHAHU <98395726+DICEALPHAHU@users.noreply.github.com>
2023-04-05 01:15:26 +0800

    Update alphahubot2.java

--- src/main/java/com/alphahubotsearcher/searcher/alphahubot2.java
+++ src/main/java/com/alphahubotsearcher/searcher/alphahubot2.java
@@ -6,19 +6,24 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.ChatColor;
 
 public class alphahubot2 extends JavaPlugin implements Listener {
 
     @Override // 加载插件
-    public void onLoad() { }
+    public void onLoad() {
+    }
 
     @Override // 启用插件
-    public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
+    public void onEnable() {        
+        this.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "[AlphahuBotSearcher]插件已成功启动，本插件由DICEALPHAHU制作");
+        this.getServer().getPluginManager().registerEvents(new MessageListener(), this);
     }
 
     @Override // 禁用插件
-    public void onDisable() { }
+    public void onDisable() {
+        this.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "[AlphahuBotSearcher]插件已卸载");
+    }
 
 
     @EventHandler
