3c3b75ce4855aaf3c25e84b77d48986a4df600c3
DICEALPHAHU <98395726+DICEALPHAHU@users.noreply.github.com>
2023-04-05 01:56:35 +0800

    添加了查询玩家的功能

--- src/main/java/com/alphahubotsearcher/searcher/alphahubot2.java
+++ src/main/java/com/alphahubotsearcher/searcher/alphahubot2.java
@@ -1,5 +1,8 @@
 package com.alphahubotsearcher.searcher;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import me.dreamvoid.miraimc.api.MiraiBot;
 import me.dreamvoid.miraimc.bukkit.event.message.passive.MiraiGroupMessageEvent;
 import org.bukkit.Bukkit;
@@ -16,20 +19,38 @@ public class alphahubot2 extends JavaPlugin implements Listener {
 
     @Override // 启用插件
     public void onEnable() {        
-        this.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "AlphahuBotSearcher is running, created by DICEALPHAHU");
+        this.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "[ABS]AlphahuBotSearcher已在运行，插件作者DICEALPHAHU，感谢MiraiMC与Shpries提供代码参考");
+        this.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "[ABS]Github.com/DICEALPHAHU");
         this.getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override // 禁用插件
     public void onDisable() {
-        this.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "AlphahuBotSearcher was disabled");
+        this.getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "[ABS]AlphahuBotSearcher已卸载");
     }
 
 
     @EventHandler
-    public void onGroupMessageReceive(MiraiGroupMessageEvent e){
+    public void onGroupMessageReceiveMap(MiraiGroupMessageEvent e){
         if(e.getMessage().equals("#卫星地图")) {
             MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage("why13700.com:37885");
         }
     }
+    
+    @EventHandler
+    public void onGroupMessageReceivePlayers(MiraiGroupMessageEvent var1) {
+        if (var1.getMessage().equals("#人数")) {
+            MiraiBot.getBot(var1.getBotID()).getGroup(var1.getGroupID()).sendMessage("服务器在线玩家数：" + Bukkit.getServer().getOnlinePlayers().size());
+            List var2 = (List)Bukkit.getOnlinePlayers();
+            List<String> var3 = new ArrayList();
+            Iterator var4 = var2.iterator();
+
+            while(var4.hasNext()) {
+                Object var5 = var4.next();
+                String var6 = ((Player)var5).getName();
+                var3.add(var6);
+            }
+
+            MiraiBot.getBot(var1.getBotID()).getGroup(var1.getGroupID()).sendMessage("目前在线玩家: " + var3);
+     }
 }
