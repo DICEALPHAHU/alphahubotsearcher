2692035318d401f4fbac3e1f17ccff3d50f7776e
DICEALPHAHU <98395726+DICEALPHAHU@users.noreply.github.com>
2023-04-05 02:06:10 +0800

    Update alphahubot2.java

--- src/main/java/com/alphahubotsearcher/searcher/alphahubot2.java
+++ src/main/java/com/alphahubotsearcher/searcher/alphahubot2.java
@@ -6,6 +6,7 @@ import java.util.List;
 import me.dreamvoid.miraimc.api.MiraiBot;
 import me.dreamvoid.miraimc.bukkit.event.message.passive.MiraiGroupMessageEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
